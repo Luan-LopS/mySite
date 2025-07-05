03a820bd28c2e6751039f64013aa707883ee8848
Luan-LopS <luam10@hotmail.com.br>
2025-07-05 00:07:10 -0300

    realizado ajuste em url

--- mySite/mySite/urls.py
+++ mySite/mySite/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', include('blog.urls'))
+    path('', lambda request: redirect('/home/', permanent=False)),
+    path('home/', include('blog.urls'))
 ]
